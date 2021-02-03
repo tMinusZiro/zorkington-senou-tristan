7b3770ea91463b552e2e7a999dd54912ee315a4a
Tristan Nelson <Tristan@tristans-mbp.lan>
2021-02-03 15:13:24 -0500

    First commit. Working on some possible data structures.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -7,6 +10,79 @@ function ask(questionText) {
   });
 }
 
+//room class
+class Room {
+  constructor(newInfo) {
+    this.secretItem = "key";
+    this.newInfo = newInfo;
+  }
+
+  hidden() {}
+
+  read() {
+    console.log(this.items[signs][welcome]);
+  }
+
+  items() {}
+
+  exit() {
+    if (this.secretItem === "key") {
+      console.log(`The key has been accepted and the door opened`);
+    } else {
+      console.log("You are still stuck in this room");
+    }
+  }
+}
+
+//experimental nonclass object
+let signs = {
+  prompts: {
+    signs: {
+      welcome:
+        "The sign says 'Welcome to Burlington Code Academy!'\nCome on up to the third floor.\nIf the door is locked, use the code 12345.",
+    },
+  },
+
+  read: () => {
+    return this.signs[prompts][signs][welcome];
+  },
+};
+
+//player class
+
+class Player {
+  constructor(
+    defaultStatus = "brave",
+    nextStatus = ["brave", "tired", "excited", "scared"]
+  ) {
+    this.status = defaultStatus;
+    this.inventory = inventory;
+  }
+
+  playerStatus() {}
+
+  playerInventory() {}
+}
+
+//game class
+class Game {
+  constructor(objective) {
+    this.objective = {
+      roomOne: "locked",
+      roomTwo: "locked",
+      roomThree: "locked",
+      roomFour: "locked",
+      roomFive: "locked",
+    };
+  }
+
+  gameStatus() {
+    if (this.objective[0] === "locked") {
+      console.log(`Room One has been unlocked.`);
+    } //build out a loop that checks if the rooms have been unlocked and when all of them have and than game over
+    //can also build out other game objectives to check for
+  }
+}
 start();
 
 async function start() {
@@ -15,6 +91,6 @@ You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign.`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  console.log("Now write your code to make this work!");
   process.exit();
 }
